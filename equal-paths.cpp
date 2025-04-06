e127a95023eb3f7099bf715fe6d90105d6cae26b
Jushen Dhillon <jsdhillo@usc.edu>
2025-04-06 07:14:56 +0000

    this should be correct

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,32 +1,65 @@
-//-----------------------------------------
-// You may NOT change anything in this file
-//-----------------------------------------
-#ifndef EQUAL_PATHS_H
-#define EQUAL_PATHS_H
-
 #ifndef RECCHECK
-#include <cstdlib>
+//if you want to add any #includes like <iostream> you must do them here (before the next endif)
+#include <utility>
 #endif
 
-struct Node {
-    int key;
-    Node *left, *right;
-    // Constructor for convenience
-    Node(int k, Node* lt = nullptr, Node* rt = nullptr) :
-        key(k), left(lt), right(rt)
-    {}
-};
+#include "equal-paths.h"
+using namespace std;
 
-/**
- * @brief Returns true if all paths from leaves to root are the same length (height),
- *        and false otherwise
- *
- *        Note: this doesn't mean we are checking if the tree is full, but just that
- *        any leaf node (wherever it may exist) has the same length path to the root 
- *        as all others.
- * 
- * @param root Pointer to the root of the tree to check for equal paths
- */
-bool equalPaths(Node* root);
 
-#endif
+// You may add any prototypes of helper functions here
+
+pair<bool,int> realEqualPaths(Node* root, int pathLengthCounter)
+{
+
+	// Check for null root
+    if (root == nullptr) {
+        return pair<bool, int>(true, 0);
+    }
+
+    // Base case
+	if (root->left == nullptr && root->right == nullptr) {
+		return pair<bool, int>(true, pathLengthCounter);
+	}
+
+	//initialization of right & left subtree
+	pair<bool,int> leftSubtree = {false, 0};
+	pair<bool,int> rightSubtree = {false, 0};
+
+	if (root->left) {
+		leftSubtree = realEqualPaths(root->left, pathLengthCounter + 1);
+	}
+	if (root->right) {
+		rightSubtree = realEqualPaths(root->right, pathLengthCounter + 1);
+	}
+
+	if (leftSubtree.second == 0) {
+		return pair<bool, int>(rightSubtree.first, rightSubtree.second);
+	} 
+	if (rightSubtree.second == 0) {
+		return pair<bool, int>(leftSubtree.first, leftSubtree.second);
+	}
+	//previous two if blocks check if there is EITHER no left or no right subtree
+
+	if (leftSubtree.first == true && rightSubtree.first == true) {
+		if (leftSubtree.second == rightSubtree.second) {
+			return pair<bool, int>(true, leftSubtree.second); //being handed off to parent to check against next subtree
+		}
+		else {
+			return pair<bool, int>(false, -1);
+		}
+	}
+	else {
+		return pair<bool, int>(false, -1); //-1 path length indicates unequal lengths
+	}
+}
+
+
+bool equalPaths(Node* root)
+{
+	if (root == nullptr) {
+		return true;
+	}
+    pair<bool, int> answerPair = realEqualPaths(root, 0);
+	return answerPair.first;
+}
